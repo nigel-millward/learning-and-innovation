c09e182b3637e5f22f65498c1c50f958ad122737
Nigel Millward <nigel.millward@bbc.com>
2024-04-06 06:04:20 +0100

    add snowpark scripts

---
+++ snowpark/src/libs/session.py
@@ -0,0 +1,16 @@
+from snowflake.snowpark import Session
+
+
+class SnowflakeSession:
+    connection_parameters = {
+        "account": "bbcstudios_test.eu-west-1",
+        "user": "nigel.millward@bbc.com",
+        "password": "<password>",
+        "role": "ACCOUNTADMIN",  # optional
+        "warehouse": "SDP_ENGINEERING_WH",  # optional
+        "database": "SDP_BRONZE",  # optional
+        "schema": "GAM_TEST",  # optional
+    }
+
+    def create(self):
+        return Session.builder.configs(self.connection_parameters).create()
