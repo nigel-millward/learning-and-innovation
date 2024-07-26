9a3bfaa9dfbb083294fc420592e7e465f7202282
Nigel Millward <nigel.millward@bbc.com>
2024-07-26 15:04:50 +0100

    add dagster assets

--- dagster_university/code_example/resources/__init__.py
+++ dagster_university/code_example/resources/__init__.py
@@ -2,5 +2,5 @@ from dagster_duckdb import DuckDBResource
 from dagster import EnvVar
 
 database_resource = DuckDBResource(
-    database=EnvVar("DUCKDB_DATABASE")      # replaced with environment variable
+    database=EnvVar("DUCKDB_DATABASE"),
 )
--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta
 import requests
-from . import constants
+import os
 
+from dagster_duckdb import DuckDBResource
 from dagster import asset
+import duckdb
+import pandas as pd
 
+from . import constants
 
 @asset
 def taxi_trips_file() -> None:
@@ -29,3 +34,91 @@ def taxi_zones_file() -> None:
 
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
+
+
+@asset(
+    deps=["taxi_trips_file"]
+)
+def taxi_trips(database: DuckDBResource) -> None:
+    sql_query = """
+        create or replace table taxi_trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(sql_query)
+
+
+@asset(
+    deps=["taxi_zones_file"]
+)
+def taxi_zones(database: DuckDBResource) -> None:
+    sql_query = f"""
+        create or replace table zones as (
+            select
+                LocationID as zone_id,
+                zone,
+                borough,
+                the_geom as geometry
+            from '{constants.TAXI_ZONES_FILE_PATH}'
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(sql_query)
+
+@asset(
+    deps=["taxi_trips"]
+)
+def trips_by_week(database: DuckDBResource) -> None:
+    current_date = datetime.strptime("2023-03-01", constants.DATE_FORMAT)
+    end_date = datetime.strptime("2023-04-01", constants.DATE_FORMAT)
+
+    result = pd.DataFrame()
+
+    while current_date < end_date:
+        current_date_str = current_date.strftime(constants.DATE_FORMAT)
+        query = f"""
+            select
+                vendor_id, total_amount, trip_distance, passenger_count
+            from trips
+            where date_trunc('week', pickup_datetime) = date_trunc('week', '{current_date_str}'::date)
+        """
+
+        with database.get_connection() as conn:
+            data_for_week = conn.execute(query).fetch_df()
+
+        aggregate = data_for_week.agg({
+            "vendor_id": "count",
+            "total_amount": "sum",
+            "trip_distance": "sum",
+            "passenger_count": "sum"
+        }).rename({"vendor_id": "num_trips"}).to_frame().T # type: ignore
+
+        aggregate["period"] = current_date
+
+        result = pd.concat([result, aggregate])
+
+        current_date += timedelta(days=7)
+
+    # clean up the formatting of the dataframe
+    result['num_trips'] = result['num_trips'].astype(int)
+    result['passenger_count'] = result['passenger_count'].astype(int)
+    result['total_amount'] = result['total_amount'].round(2).astype(float)
+    result['trip_distance'] = result['trip_distance'].round(2).astype(float)
+    result = result[["period", "num_trips", "total_amount", "trip_distance", "passenger_count"]]
+    result = result.sort_values(by="period")
+
+    result.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
