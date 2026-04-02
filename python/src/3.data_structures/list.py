419688a6cd6900cb5123dc738eb973ff946b90d4
nigel millward <nigel.millward@bbc.com>
2026-04-02 08:19:36 +0100

    update lists

--- python/src/3.data_structures/list.py
+++ python/src/3.data_structures/list.py
@@ -2,6 +2,19 @@
 squares = [1, 4, 9, 16, 25]
 print(squares)
 
+# start with an empty list and append
+users = []
+users.append('val')
+users.append('bob')
+users.append('mia')
+users.insert(3, 'bea') # append by position
+users[0] = 'valentina'  # change value by index
+
+# delete items from list
+del users[-1]  # delete last item
+users.remove('bob')  # delete by value
+users.pop(0)  # delete by index and return the value
+
 #index & slice list
 squares[0]  # first element
 squares[-1] # last element
@@ -41,3 +54,10 @@ x = [a, n]
 # list comprehensions - [expression for item in iterable if condition]
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+# sorting a list
+users = ['val', 'bob', 'mia', 'ron', 'ned']
+users.sort()  # sorts the list permanently
+users.sort()(reverse=True)  # sorts in reverse order
+
+
